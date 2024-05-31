fb74ff7e2898779df6afe26e7b09deaa0c9e09df
Parikesittt <parikesit.widodo@gmail.com>
2024-05-31 11:51:07 +0700

    update

---
+++ app/src/main/java/com/example/cornache/adapter/ChatAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.cornache.adapter
+
+class ChatAdapter {
+}
---
+++ app/src/main/java/com/example/cornache/adapter/HistoryAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.cornache.adapter
+
+import androidx.recyclerview.widget.ListAdapter
+
+class HistoryAdapter {
+}

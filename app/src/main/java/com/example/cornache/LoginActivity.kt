337d6e30780e29a89d4d99c3a35247302d2fcf23
Arkade7 <vedo.priyantoro@gmail.com>
2024-06-12 06:49:58 +0700

    b4 pull

--- app/src/main/java/com/example/cornache/LoginActivity.kt
+++ app/src/main/java/com/example/cornache/LoginActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
     }
 }
